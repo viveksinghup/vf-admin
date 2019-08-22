5ec72f8d242e1340e4c5d52eb0cbaf45795ae344
Vivek Singh <viveksinghup.singh@gmail.com>
2019-08-22 16:06:00 +0530

    new compoment added and scroll always on top added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -17,6 +16,9 @@ import { InvoiceComponent } from './invoice/invoice.component';
 import { ErrorComponent } from './error/error.component';
 import { ResetComponent } from './reset/reset.component';
 import { AddproductComponent } from './addproduct/addproduct.component';
+import { AllproductsComponent } from './allproducts/allproducts.component';
+import { NotificationComponent } from './notification/notification.component';
+import { SettingComponent } from './setting/setting.component';
 
 
 const routes: Routes = [
@@ -24,10 +26,13 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent },
   {path: 'reset', component: ResetComponent },
   {path: 'dashboard', component: HomeComponent },
-  {path: 'error', component: ErrorComponent },
   {path: 'all-orders', component: OrderComponent },
   {path: 'add-product', component: AddproductComponent },
   {path: 'invoice', component: InvoiceComponent },
+  {path: 'all-products', component: AllproductsComponent },
+  {path: 'notification', component: NotificationComponent },
+  {path: 'setting', component: SettingComponent },
+  {path: '**', component: ErrorComponent },
 ]
 
 @NgModule({
@@ -42,7 +47,10 @@ const routes: Routes = [
     InvoiceComponent,
     ErrorComponent,
     ResetComponent,
-    AddproductComponent
+    AddproductComponent,
+    AllproductsComponent,
+    NotificationComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
